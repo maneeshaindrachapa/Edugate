44388aefdfc971542bd21cf199219018bd721199
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-03-24 21:27:11 +0530

    UI Changes

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
-
+  rate = 3.5;
   constructor() { }
 
   ngOnInit() {
