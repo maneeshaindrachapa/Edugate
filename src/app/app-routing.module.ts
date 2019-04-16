7a875d39bb2dc50153ba207a1cbceb6985b0a7f6
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-04-16 17:45:14 +0530

    new routings added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [{path: '' , component: HomepageComponent}];
+const routes: Routes = [{ path: '', component: HomepageComponent }, { path: 'login', component: LoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
