e1c89458d76fbcefdf5639ab54eb9b7e07082cd5
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-04-17 17:31:59 +0530

    Register function added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,12 +8,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  user = { username: '', email: '', password: '', retype: '' };
+  constructor(private router: Router, private auth: AuthService) { }
 
   ngOnInit() {
   }
   login() {
     this.router.navigate(['login']);
+  }
+  register() {
+    if (this.user.username !== '' && this.user.email !== '' && this.user.password !== '' && this.user.retype !== '') {
+      this.auth.register(this.user.username, this.user.email, this.user.password, this.user.retype).subscribe(Sucess => {
+        console.log(Sucess);
+      }, error => {
+        console.log(error);
+      });
+    }
   }
 }
