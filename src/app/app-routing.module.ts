c6e38e81d4abe88ba4ba716a778fb3da4c91fc2e
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-04-17 13:40:55 +0530

    add 'register' routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [{ path: '', component: HomepageComponent }, { path: 'login', component: LoginComponent }];
+const routes: Routes = [{ path: '', component: HomepageComponent }, { path: 'login', component: LoginComponent }, { path: 'register', component: RegisterComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
