8db461862513d6dcfa1e5839fe8a5afb5953899d
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-04-18 20:08:23 +0530

    navbar UI fixes

--- src/app/dashboard/dashboard-navbar/dashboard-navbar.component.ts
+++ src/app/dashboard/dashboard-navbar/dashboard-navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
 import * as jwt_decode from 'jwt-decode';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-navbar',
@@ -9,11 +10,45 @@ import { Router } from '@angular/router';
 })
 export class DashboardNavbarComponent implements OnInit {
   username = '';
-  constructor(private router: Router) {
+  private toggleButton: any;
+  private sidebarVisible: boolean;
+  constructor(private router: Router, public location: Location, private element: ElementRef) {
+    this.sidebarVisible = false;
   }
 
   ngOnInit() {
     this.username = this.getDecodedAccessToken(localStorage.getItem('token'))['username'];
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+  }
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const html = document.getElementsByTagName('html')[0];
+    // console.log(html);
+    // console.log(toggleButton, 'toggle');
+
+    setTimeout(function () {
+      toggleButton.classList.add('toggled');
+    }, 500);
+    html.classList.add('nav-open');
+
+    this.sidebarVisible = true;
+  }
+  sidebarClose() {
+    const html = document.getElementsByTagName('html')[0];
+    // console.log(html);
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    html.classList.remove('nav-open');
+  }
+  sidebarToggle() {
+    // const toggleButton = this.toggleButton;
+    // const body = document.getElementsByTagName('body')[0];
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
+    }
   }
   getDecodedAccessToken(token: string): any {
     try {
@@ -25,5 +60,11 @@ export class DashboardNavbarComponent implements OnInit {
   logout() {
     localStorage.clear();
     this.router.navigate(['']);
+  }
+  edit() {
+    this.router.navigate(['editprofile']);
+  }
+  dashboard() {
+    this.router.navigate(['dashboard']);
   }
 }
