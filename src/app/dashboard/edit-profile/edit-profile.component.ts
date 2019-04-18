fd1b36002e6b7ed964d1b44b8d56fec1b26f5ec1
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-04-18 20:07:53 +0530

    edit-profile component added

---
+++ src/app/dashboard/edit-profile/edit-profile.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import * as jwt_decode from 'jwt-decode';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.css']
+})
+export class EditProfileComponent implements OnInit {
+  username = '';
+  role = '';
+  profile_pic = 'people-avatar-1.jpg'; // default prof pic
+  constructor(private auth: AuthService) { }
+
+  ngOnInit() {
+    this.role = localStorage.getItem('role');
+    this.username = this.getDecodedAccessToken(localStorage.getItem('token'))['username'];
+    console.log(this.getDecodedAccessToken(localStorage.getItem('token')));
+    console.log((this.getDecodedAccessToken(localStorage.getItem('token'))['user_id']));
+
+    // get profile details
+    if (this.role === 'student') {
+      this.auth.getStudentProfile(this.getDecodedAccessToken(localStorage.getItem('token'))['user_id']).subscribe(success => {
+        console.log(success);
+      }, error => {
+        console.log(error);
+      });
+    } else if (this.role === 'teacher') {
+      this.auth.getTeacherProfile(this.getDecodedAccessToken(localStorage.getItem('token'))['user_id']).subscribe(success => {
+        console.log(success);
+      }, error => {
+        console.log(error);
+      });
+    } else if (this.role === 'assistant') {
+      this.auth.getAssistantProfile(this.getDecodedAccessToken(localStorage.getItem('token'))['user_id']).subscribe(success => {
+        console.log(success);
+      }, error => {
+        console.log(error);
+      });
+    }
+
+  }
+  getDecodedAccessToken(token: string): any {
+    try {
+      return jwt_decode(token);
+    } catch (Error) {
+      return null;
+    }
+  }
+}
