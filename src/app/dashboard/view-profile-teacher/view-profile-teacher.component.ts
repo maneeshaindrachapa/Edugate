3a1869a6dd20946340cea862b4ffe65ba838290e
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-05-07 16:36:19 +0530

    View Profile teacher component added

---
+++ src/app/dashboard/view-profile-teacher/view-profile-teacher.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ClassService } from 'src/app/services/class.service';
+
+@Component({
+  selector: 'app-view-profile-teacher',
+  templateUrl: './view-profile-teacher.component.html',
+  styleUrls: ['./view-profile-teacher.component.scss']
+})
+export class ViewProfileTeacherComponent implements OnInit {
+  role = '';
+  profile_pic = 'people-avatar-1.jpg';
+  teacherDetails: any;
+  classes = [];
+  /* data tabel for classes */
+  rows = [];
+  columns = [
+    { prop: 'subject_id', name: 'Subject' },
+    { prop: 'batch_id', name: 'Batch' },
+    { prop: 'group_id', name: 'Group' },
+    { prop: 'day_of_week', name: 'Day' },
+    { prop: 'starting_time', name: 'Time' },
+    { prop: 'fees', name: 'Fees' }
+  ];
+  selected = [];
+  /*************/
+  constructor(private auth: AuthService, private modalService: NgbModal, private classSer: ClassService) { }
+
+  ngOnInit() {
+    this.teacherDetails = this.auth.getTeacher();
+    this.getClasses();
+  }
+  selectClass({ selected }) {
+    this.selected.splice(0, this.selected.length);
+    this.selected.push(...selected);
+  }
+  getClasses() {
+    this.classes = [];
+    this.selected = [];
+    this.classSer.getClassesOfTeacher(this.teacherDetails['teacher_id']).subscribe(success => {
+      for (let i = 0; i < success['data'].length; i++) {
+        this.classes.push(success['data'][i]);
+      }
+      this.rows = this.classes;
+    }, error => {
+      console.log(error);
+    });
+  }
+  editTeacher() {
+    this.auth.editTeacher(this.teacherDetails).subscribe(success => {
+      console.log(success);
+    }, error => {
+      console.log(error);
+    });
+  }
+}
