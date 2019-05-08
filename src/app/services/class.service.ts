87259dea516c396eb3ff6e210f069c60f64462e2
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-05-09 05:03:31 +0530

    new functions added

--- src/app/services/class.service.ts
+++ src/app/services/class.service.ts
@@ -34,12 +34,46 @@ export class ClassService {
     // tslint:disable-next-line:max-line-length
     return this.http.post('http://localhost:8080/classes/get/by-day', { 'search_word': search_word }, { headers: this.headers }).pipe(map(res => res));
   }
+  addClass(subject_id, batch_id, group_id, teacher_id, starting_date, starting_time, ending_time, fees, daily_fees) {
+    return this.http.post('http://localhost:8080/classes/add',
+      {
+        'subject_id': subject_id,
+        'batch_id': batch_id,
+        'group_id': group_id,
+        'teacher_id': teacher_id,
+        'starting_date': starting_date,
+        'ending_time': ending_time,
+        'starting_time': starting_time,
+        'fees': fees,
+        'daily_fees': daily_fees
+      },
+      { headers: this.headers }).pipe(map(res => res));
+  }
 
   /* get batch */
   getBatches() {
     return this.http.get('http://localhost:8080/classes/batches', { headers: this.headers }).pipe(map(res => res));
   }
-
+  addBatch(category, year) {
+    return this.http.post('http://localhost:8080/classes/add/batch', { 'category': category, 'year': year },
+      { headers: this.headers }).pipe(map(res => res));
+  }
+  /* get groups */
+  getGroups() {
+    return this.http.get('http://localhost:8080/classes/groups', { headers: this.headers }).pipe(map(res => res));
+  }
+  addGroup(category, name) {
+    return this.http.post('http://localhost:8080/classes/add/group', { 'category': category, 'name': name },
+      { headers: this.headers }).pipe(map(res => res));
+  }
+  /* get subjects */
+  getSubjects() {
+    return this.http.get('http://localhost:8080/classes/subjects', { headers: this.headers }).pipe(map(res => res));
+  }
+  addSubject(stream, subjectName) {
+    return this.http.post('http://localhost:8080/classes/add/subject', { 'stream': stream, 'subject': subjectName },
+      { headers: this.headers }).pipe(map(res => res));
+  }
   /* get classes of a  student */
   getClassesOfStudent(std_id) {
     return this.http.get('http://localhost:8080/students/get/classes/' + std_id, { headers: this.headers }).pipe(map(res => res));
@@ -62,5 +96,11 @@ export class ClassService {
   assignAssistant(ass_id, class_details) {
     return this.http.post('http://localhost:8080/assistants/assign/:' + ass_id,
       { 'class_info': class_details }, { headers: this.headers }).pipe(map(res => res));
+  }
+
+  /* marking*/
+  enableMarking(class_id, class_) {
+    return this.http.post('http://localhost:8080/classes/enable/:' + class_id,
+      { 'class': class_ }, { headers: this.headers }).pipe(map(res => res));
   }
 }
