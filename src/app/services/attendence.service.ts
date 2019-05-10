6627ac74b65cc3a9db458a29d92c7c79055bf11c
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-05-11 05:19:31 +0530

    attendence routes added

--- src/app/services/attendence.service.ts
+++ src/app/services/attendence.service.ts
@@ -13,7 +13,25 @@ export class AttendenceService {
   });
 
   constructor(private http: HttpClient) { }
-  markAttendence() {
 
+  getStudentAttendence(std_id) {
+    return this.http.get('http://localhost:8080/attendance/get/' + std_id, { headers: this.headers }).pipe(map(res => res));
+  }
+  public mark(std_id, class_id, class_date) {
+    return this.http.post('http://localhost:8080/attendance/mark',
+      {
+        'student_id': std_id,
+        'class_id': class_id,
+        'class_date': class_date
+      }, { headers: this.headers }).pipe(map(res => res));
+  }
+  public top_up(std_id, class_id, for_, amount) {
+    return this.http.post('http://localhost:8080/attendance/topup',
+      {
+        'student_id': std_id,
+        'class_id': class_id,
+        '_for': for_,
+        'amount': amount
+      }, { headers: this.headers }).pipe(map(res => res));
   }
 }
