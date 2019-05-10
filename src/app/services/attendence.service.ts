27dd79339ee870c8bda40a7a030fbc88f2187867
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-05-10 17:08:51 +0530

    Attendence Service created

---
+++ src/app/services/attendence.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AttendenceService {
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Authorization': 'Bearer ' + localStorage.getItem('token')
+  });
+
+  constructor(private http: HttpClient) { }
+  markAttendence() {
+
+  }
+}
